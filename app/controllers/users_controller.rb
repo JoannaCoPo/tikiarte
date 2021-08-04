1c486005f25c20063fa2904bacca7528bd1dfcec
JoannaCoPo <jococopo@gmail.com>
2021-08-04 13:18:09 -0600

    Define new and create users actions

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,25 @@
 class UsersController < ApplicationController
+  def new
+    @user = User.new
+  end
+
+  def create
+    user = user_params
+    user[:email] = user[:email].downcase
+    new_user = User.create(user)
+    if new_user.save
+      flash[:success] = "Welcome, #{new_user.first_name}"
+      session[:user_id] = new_user.id
+      redirect_to dashboard_path
+    else
+      flash[:error] = new_user.full_messages.join(', ')
+      redirect_to new_user_path
+    end
+  end
+
+  private
+
+  def user_params
+    params.permit(:email, :password, :password_confirmation, :first_name, :last_name)
+  end
 end
