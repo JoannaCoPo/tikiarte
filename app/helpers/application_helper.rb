92030ad03208e7232eabbbb07e65c157f8355f37
JoannaCoPo <jococopo@gmail.com>
2021-08-07 15:58:27 -0600

    Style flash messages via Bootstrap

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,12 @@
 module ApplicationHelper
+  def flash_class(level)
+    case level
+    when 'notice'
+      "class='alert alert-info text-center'".html_safe
+    when 'success'
+      "class='alert alert-success text-center'".html_safe
+    when 'alert'
+      "class='alert alert-danger text-center'".html_safe
+    end
+  end
 end
