a78c58f35e4d29b728015756b18a696b3a45263b
JoannaCoPo <jococopo@gmail.com>
2021-08-07 19:44:11 -0600

    Create sessions controller with new, create, and destrouy actions

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,20 @@
+class SessionsController < ApplicationController
+  def new; end
+
+  def create
+    found_user = User.find_by(email: params[:email])
+    if found_user && found_user.authenticate(params[:password])
+      session[:user_id] = found_user.id
+      flash[:alert] = "Welcome back, #{found_user.first_name}!"
+      redirect_to dashboard_path
+    else
+      flash[:alert] = 'Your username or password are incorrect. Please try again.'
+      redirect_to login_path
+    end
+  end
+
+  def destroy
+    session[:user_id] = nil
+    redirect_to '/'
+  end
+end
