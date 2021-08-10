61012c3a4014aada4ff0a34bc332df6ef7dcceae
JoannaCoPo <jococopo@gmail.com>
2021-08-09 19:30:07 -0600

    Add test for invalid login

--- spec/features/users/sessions_spec.rb
+++ spec/features/users/sessions_spec.rb
@@ -7,18 +7,50 @@ RSpec.describe "Login Page" do
       visit root_path
     end
 
-    it 'it allows registered users to log in with correct credentials' do
-      click_link 'Log me in!'
+    describe 'form,' do
+      it 'has email and password fields' do
 
-      expect(current_path).to eq(new_login_path)
+        expect(page).to have_link('Log me in!')
 
-      fill_in :email, with: @user_1.email
-      fill_in :password, with: @user_1.password
+        click_link 'Log me in!'
 
-      click_button('Log In')
+        expect(current_path).to eq(login_path)
+        expect(page).to have_field('Email')
+        expect(page).to have_field('Password')
+      end
 
-      expect(current_path).to eq(dashboard_path)
-      expect(page).to have_content("Welcome back, #{@user_1.first_name}!")
+      context 'valid login,' do
+        it 'it allows registered users to log in with correct credentials' do
+          click_link 'Log me in!'
+
+          expect(current_path).to eq(login_path)
+
+          fill_in :email, with: @user_1.email
+          fill_in :password, with: @user_1.password
+          click_button('Log In')
+
+          expect(current_path).to eq(dashboard_path)
+          expect(page).to have_content("Welcome back, #{@user_1.first_name}!")
+        end
+      end
+
+      context 'invalid login,' do
+        it 'does not allow users to log in with invlaid credentials' do
+          incorrect_password = 'hocuslocus'
+
+          click_link 'Log me in!'
+
+          expect(current_path).to eq(login_path)
+
+          fill_in :email, with: @user_1.email
+          fill_in :password, with: incorrect_password
+          save_and_open_page
+          click_button('Log In')
+
+          expect(current_path).to eq(login_path)
+          expect(page).to have_content("Your username or password are incorrect")
+        end
+      end
     end
   end
 end
