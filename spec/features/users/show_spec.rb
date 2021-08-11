75da76277617aee02ba60e5299b3fd8467c71d83
JoannaCoPo <jococopo@gmail.com>
2021-08-11 15:33:09 -0600

    Test that it shows when no kidshave been added

--- spec/features/users/show_spec.rb
+++ spec/features/users/show_spec.rb
@@ -6,21 +6,46 @@ RSpec.describe "Show Page" do
     @kid_1 = Kid.create!(user: @user_1, first_name: 'Scarlett', last_name: 'Cortes')
     @kid_2 = Kid.create!(user: @user_1, first_name: 'Mystery', last_name: 'Child')
     allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(@user_1)
-    visit dashboard_path
+    visit user_path
   end
 
   describe 'user kids section' do
-    xit 'displays kids that user has added to account' do
+    it 'displays kids that user have been added to account' do
       within '#kids' do
-
-        expect(page).to have_content(@kid_1.name)
-        expect(page).to have_content(@kid_2.name)
+        expect(page).to have_content(@kid_1.first_name)
+        expect(page).to have_content(@kid_2.first_name)
       end
     end
 
-    xit 'links to the kid artwork show page' do
+    it 'links to the kid artwork show page' do
       within '#kids' do
-        expect(page).to have_button(@kid_1.name)
+        expect(page).to have_button(@kid_1.first_name)
+
+        click_button(@kid_1.first_name)
+
+        expect(current_path).to eq(user_kid_path)
+      end
+    end
+
+    it 'has link to add another kid to user account' do
+      within '#kids' do
+        expect(page).to have_button('Add another kid')
+        click_button('Add another kid')
+        expect(current_path).to eq(new_user_kid_path)
+      end
+    end
+
+    it 'indicates when no kids have been added yet' do
+      @user_2 = User.create!(email: 'test@gmail.com', password: 'test', first_name: 'Test', last_name: 'Tester')
+      allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(@user_2)
+      visit user_path
+
+      within '#kids' do
+        expect(page).to have_content("You haven't added any kids to your account yet.")
+        expect(page).to have_button('Add kid')
+
+        click_button('Add kid')
+        expect(current_path).to eq(new_user_kid_path)
       end
     end
   end
